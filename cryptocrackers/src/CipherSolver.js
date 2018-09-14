3b2e265dd1c7231e8c18cd55331886e07e8eb0f4
Rolf Michelsen <rolf@rolfmichelsen.com>
2018-09-14 11:44:45 +0200

    Recfactoring component organization

---
+++ cryptocrackers/src/CipherSolver.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TextInput from "./TextInput";
+import TextOutput from "./TextOutput";
+import Cipher from "./Cipher";
+
+
+class CipherSolver extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>{this.props.cipherName}</h1>
+                <p>bla bla bla...</p>
+                <TextInput caption="Ciphertext" value={this.props.cipher.ciphertext} onTextChange={this.props.onCiphertextChange} />
+                <TextOutput caption="Plaintext" value={this.props.cipher.plaintext} />
+                <fieldset>
+                    <legend>Solver</legend>
+                    {this.props.cipherSolver}
+                </fieldset>
+            </div>
+        );
+    }
+}
+
+
+CipherSolver.propTypes = {
+    cipherName: PropTypes.string.isRequired,                    // Name of cipher
+    cipherText: PropTypes.element.isRequired,                   // Text description of cipher
+    cipherSolver: PropTypes.element.isRequired,                 // concrete solver for the cipher
+    cipher: PropTypes.instanceOf(Cipher).isRequired,            // Class providing the actual cipher functionality
+    onCiphertextChange: PropTypes.func,                         // callback for changes to the ciphertext
+};
+
+
+export default CipherSolver;
--- cryptocrackers/src/Substitution.js
+++ cryptocrackers/src/Substitution.js
@@ -1,14 +1,117 @@
 import React from "react";
+import PropTypes from "prop-types";
+import CipherSolver from "./CipherSolver";
+import SubstitutionCipher from "./SubstitutionCipher";
+import "./Substitution.css";
 
 
 class Substitution extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        const cipher = new SubstitutionCipher();
+        cipher.ciphertext = "Enter some text...";
+
+        this.state = {
+            cipher: cipher,
+        };
+
+        this.handleCiphertextChange = this.handleCiphertextChange.bind(this);
+        this.handleCipherkeyChange = this.handleCipherkeyChange.bind(this);
+    }
+
+
+    handleCiphertextChange(text) {
+        this.setState((state) => {
+            const cipher = state.cipher;
+            cipher.ciphertext = text;
+            return ({
+                cipher: cipher,
+            });
+        });
+    }
+
+
+    handleCipherkeyChange(key) {
+        this.setState((state) => {
+            const cipher = state.cipher;
+            cipher.key = key;
+            return ({
+                cipher: cipher,
+            });
+        });
+    }
+
+
     render() {
         return (
-            <p>Substitition cipher goes here...</p>
+            <CipherSolver
+                cipherName="Substition cipher"
+                cipherText={<p>bla bla bla...</p>}
+                cipherSolver={<SubstitutionSolver cipher={this.state.cipher} onCipherKeyChange={this.handleCipherkeyChange} />}
+                cipher={this.state.cipher}
+                onCiphertextChange={this.handleCiphertextChange} />
         );
     }
 }
 
 
 export default Substitution;
+
+
+class SubstitutionSolver extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleKeyChange = this.handleKeyChange.bind(this);
+    }
+
+
+    countCharacterOccurrences(text) {
+        const occ = {};
+        for (let ch of text) {
+            let count = (ch in occ) ? occ[ch]+1 : 1;
+            occ[ch] = count;
+        }
+        return occ;
+    }
+
+
+    handleKeyChange(event, char) {
+        const key = this.props.cipher.key;
+        key[event.target.value] = char;
+        this.props.onCipherKeyChange(key);
+        console.log("New key mapping: " + char + " -> " + event.target.value)
+    }
+
+
+    render() {
+        const occ = this.countCharacterOccurrences(this.props.cipher.ciphertext);
+        const table = [];
+        for (const ch in occ) {
+            table.push(
+                <tr key={ch}>
+                    <td className="char">{ch}</td>
+                    <td className="occurrenceVisual"><progress max="100" value={occ[ch]}>{occ[ch]}</progress></td>
+                    <td className="occurenceCount">{occ[ch]}</td>
+                    <td className="substitution"><input maxLength={1} size={2} onChange={(e) => this.handleKeyChange(e, ch)} /></td>
+                </tr>
+            );
+        }
+        return (
+            <table className="characterOccurrences">
+                <tbody>
+                    {table}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+
+SubstitutionSolver.propTypes = {
+    cipher: PropTypes.instanceOf(SubstitutionCipher).isRequired,
+    onCipherKeyChange: PropTypes.func.isRequired,
+};
